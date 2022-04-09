5f31b7c09a86f27be8a581961996d8defafd6e73
BOHDANff <popivbod12@gmail.com>
2022-04-09 16:41:16 +0300

    Creating snake wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useCallback, useRef, useState} from "react";
+import useResizeObserver from "@react-hook/resize-observer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const root = useRef(null);
+    const [width, setWidth] = useState();
+    useResizeObserver(root, entry => {
+        let rootWidth = entry.contentRect.width
+        if (rootWidth>1300) {
+            setWidth((0.4 * rootWidth))
+        } else if (rootWidth>1200) {
+            setWidth((0.5 * rootWidth))
+        } else if (rootWidth>900) {
+            setWidth((0.55 * rootWidth))
+        } else if (rootWidth>700) {
+            setWidth((0.65 * rootWidth))
+        } else {
+            setWidth((0.8 * rootWidth))
+        }
+        }
+    )
+    return (
+    <div className="App" ref={root}>
+        <div className="snake__wrapper" style={{width: `${width}px`,
+            height: `${width}px`}}>
+        </div>
+
     </div>
   );
 }
